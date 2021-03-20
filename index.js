10535cd4d4155e00fbc509abefd877f866b3dc2e
Patrick Simmons <hadroncollides@gmail.com>
2021-03-20 19:54:47 -0400

    commit tree 5c2aa2111cbcb2926f4b621c7b5e26cb8ab7c1d3

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(bl) {
+    if (bl > 42) {
+        return (bl - 42);
+    } else {
+        return (42 - bl);
+    }
+}
+
+function distanceFromHqInFeet(bl) {
+    return (distanceFromHqInBlocks(bl) * 264);
+}
+
+function distanceTravelledInFeet(a, b) {
+    distance = a-b;
+    return distance * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 2500) {
+        return "cannot travel that far";
+    } else if (distance > 2000) {
+        return 25;
+    } else {
+        return distance * 0.02;
+    }
+}
